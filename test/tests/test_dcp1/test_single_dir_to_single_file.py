7d12f1774fb2ea4867c516d4a527de58843e985e
Danielle Sikich <sikich1@llnl.gov>
2017-01-12 14:41:53 -0800

    missed some legacy test files that still said dcp instead of dcp1

--- test/tests/test_dcp1/test_single_dir_to_single_file.py
+++ test/tests/test_dcp1/test_single_dir_to_single_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
 from subprocess import call
 
-def test_dcp_single_dir_to_single_file():
-        rc = call("~/fileutils.git/test/legacy/dcp_tests/test_dcp_single_dir_to_single_file/test.sh", shell=True)
+def test_dcp1_single_dir_to_single_file():
+        rc = call("~/fileutils.git/test/legacy/dcp1_tests/test_dcp1_single_dir_to_single_file/test.sh", shell=True)
--- test/tests/test_dcp1/test_single_file_to_single_dir.py
+++ test/tests/test_dcp1/test_single_file_to_single_dir.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
 from subprocess import call
 
-def test_dcp_single_file_to_single_dir():
-        rc = call("~/fileutils.git/test/legacy/dcp_tests/test_dcp_single_file_to_single_dir/test.sh", shell=True)
+def test_dcp1_single_file_to_single_dir():
+        rc = call("~/fileutils.git/test/legacy/dcp1_tests/test_dcp1_single_file_to_single_dir/test.sh", shell=True)
