a15504572c3694f2d9a10da5c2382209d829d50f
Dalton Bohning <daltonx.bohning@intel.com>
2020-09-03 23:03:35 +0000

    Rough draft of new error codes
    Signed-off-by: Dalton Bohning <daltonx.bohning@intel.com>

---
+++ src/common/mfu_errors.h
@@ -0,0 +1,27 @@
+/* Defines common error codes */
+
+/* enable C++ codes to include this header directly */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef MFU_ERRORS_H
+#define MFU_ERRORS_H
+
+/* Generic error codes */
+#define ERR_INVAL_ARG EINVAL // 22
+
+/* DAOS specific error codes*/
+#define ERR_DAOS            40
+#define ERR_DAOS_INVAL_POOL 41
+#define ERR_DAOS_INVAL_CONT 42
+#define ERR_DAOS_INVAL_SVCL 43
+#define ERR_DAOS_INVAL_PRFX 44
+#define ERR_DAOS_ARG_REQ    45
+
+#endif /* MFU_ERRORS_H */
+
+/* enable C++ codes to include this header directly */
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
