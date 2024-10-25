8e76635cbf5dd2bf4cd1ef926710e19bbf6ad494
Olaf Faaland <faaland1@llnl.gov>
2024-10-24 18:43:05 -0700

    test_dsync: add test_expectfail.sh and test_existence.sh and stubs
    Add the following tests:
    	test_expectfail.sh: add tests for missing source or destination parent
    	test_existence.sh: verify set of files synced is correct
    
    Add stubs for the following tests:
    	test_metadata.sh: verify metadata of synced files is correct
    	test_data.sh: verify data in synced files is correct
    
    Signed-off-by: Olaf Faaland <faaland1@llnl.gov>

---
+++ test/tests/test_dsync/test_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##############################################################################
+# Description:
+#
+#   Verify dsync handles data
+#     - file data is identical after a copy
+#     - file with differing data is copied if --contents arg is used
+#     - file with differing data is not copied if --contents arg is not used and metadata match
+#
+# Notes:
+#   - does not test whether data copes are spread across nodes/tasks evenly
+#
+##############################################################################
+
+# Turn on verbose output
+#set -x
+
+DSYNC_TEST_BIN=${DSYNC_TEST_BIN:-${1}}
+DSYNC_SRC_DIR=${DSYNC_SRC_DIR:-${2}}
+DSYNC_DEST_DIR=${DSYNC_DEST_DIR:-${3}}
+DSYNC_TMP_FILE=${DSYNC_TMP_FILE:-${4}}
+
+echo "Using dsync binary at: $DSYNC_TEST_BIN"
+echo "Using src directory at: $DSYNC_SRC_DIR"
+echo "Using dest directory at: $DSYNC_DEST_DIR"
+echo "Using directory tree: $DSYNC_TMP_FILE"
+
+# tests not yet implemented
+
+exit 0
---
+++ test/tests/test_dsync/test_expectfail.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+. utility/set_funcs.sh
+
+##############################################################################
+# Description:
+#
+#   Verify dsync fails when it should
+#      - source does not exist
+#      - parent of dest does not exist
+#
+# Notes:
+#
+##############################################################################
+
+# Turn on verbose output
+#set -x
+
+DSYNC_TEST_BIN=${DSYNC_TEST_BIN:-${1}}
+DSYNC_SRC_BASE=${DSYNC_SRC_BASE:-${2}}
+DSYNC_DEST_BASE=${DSYNC_DEST_BASE:-${3}}
+DSYNC_TREE_NAME=${DSYNC_TREE_NAME:-${4}}
+
+echo "Using dsync binary at: $DSYNC_TEST_BIN"
+echo "Using src parent directory at: $DSYNC_SRC_BASE"
+echo "Using dest parent directory at: $DSYNC_DEST_BASE"
+
+DSYNC_SRC_DIR=$(mktemp --directory ${DSYNC_SRC_BASE}/${DSYNC_TREE_NAME}.XXXXX)
+DSYNC_DEST_DIR=$(mktemp --directory ${DSYNC_DEST_BASE}/${DSYNC_TREE_NAME}.XXXXX)
+
+function sync_and_verify()
+{
+	local srcdir=$1
+	local destdir=$2
+	local name=$3
+	local expectation=$4
+
+	local result=0
+	local dest_type="unknown"
+
+	quiet_opt="--quiet"
+
+	$DSYNC_TEST_BIN $quiet_opt $delete_opt $srcdir $destdir
+	rc=$?
+	echo "dsync failed with rc $rc"
+
+	if [[ "$rc" -eq 0 ]]; then
+		echo "FAILED verify of option $name for $destdir type $dest_type"
+		result=1
+
+	else
+		echo "PASSED verify of option $name for $destdir type $dest_type"
+		result=0
+	fi
+
+	return $result
+}
+
+# source does not exist
+rm -fr $DSYNC_SRC_DIR/stuff
+rm -fr $DSYNC_DEST_DIR/stuff
+mkdir $DSYNC_DEST_DIR/stuff
+sync_and_verify  $DSYNC_SRC_DIR/stuff $DSYNC_DEST_DIR/stuff source_missing expect_dsync_fail
+
+# parent of dest does not exist
+rm -fr $DSYNC_SRC_DIR/stuff
+rm -fr $DSYNC_DEST_DIR/stuff
+mkdir $DSYNC_SRC_DIR/stuff
+sync_and_verify  $DSYNC_SRC_DIR/stuff $DSYNC_DEST_DIR/stuff/childdir dest_parent_missing expect_dsync_fail
+
+# clean up
+rm -fr $DSYNC_SRC_DIR/stuff
+rm -fr $DSYNC_DEST_DIR/stuff
+
+exit 0
