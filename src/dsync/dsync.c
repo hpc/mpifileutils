27fd9194ff57769cced1999e00c12bc6c48b54a6
Olaf Faaland <faaland1@llnl.gov>
2024-11-12 12:00:18 -0800

    dsync: disable delete on src walk error
    If there is an error during the walk of the source tree, the source flist
    generated is likely incomplete.
    
    In the case where the source and destination have the same files and
    directories before dsync --delete is run, an incomplete source flist
    results in files being incorrectly deleted from the destination.
    
    When an error is detected on the source walk, warn the user and disable
    the delete option.
    
    See rsync(1) for the --delete option, it takes the same approach.

--- src/dsync/dsync.c
+++ src/dsync/dsync.c
@@ -2968,6 +2968,7 @@ dsync_daos_out:
 int main(int argc, char **argv)
 {
     int rc = 0;
+    int walk_rc = 0;
 
     /* initialize MPI and mfu libraries */
     MPI_Init(&argc, &argv);
@@ -3311,7 +3312,19 @@ int main(int argc, char **argv)
     if (rank == 0) {
         MFU_LOG(MFU_LOG_INFO, "Walking source path");
     }
-    mfu_flist_walk_param_paths(1, srcpath, walk_opts, flist_tmp_src, mfu_src_file);
+    walk_rc = mfu_flist_walk_param_paths(1, srcpath, walk_opts, flist_tmp_src, mfu_src_file);
+
+    /* If we encountered an error during the srcpath walk, the src flist is likely incomplete,
+     * and a delete might delete files already on the destination.  Disable the delete and
+     * notify the user. rsync takes the same approach. */
+    int all_rc;
+    MPI_Allreduce(&walk_rc, &all_rc, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+    if (all_rc > 0) {
+        if (options.delete == 1) {
+            MFU_LOG(MFU_LOG_WARN, "Errors detected walking source path, delete option disabled");
+            options.delete = 0;
+	}
+    }
 
     /* check that we actually got something so that we don't delete
      * an entire target directory because of a typo on the source dir */
@@ -3329,12 +3342,6 @@ int main(int argc, char **argv)
         rc = 1;
         goto dsync_common_cleanup;
     }
-
-    /* TODO:
-     * if we encountered an error during the srcpath walk, above, we should either
-     * quit or disable deletes if that option was used.
-     options.delete = 0; (1 == do deletes, 0 = do not do deletes)
-     */
 
     /* walk destinaton path.
      * We never dereference the destination */
