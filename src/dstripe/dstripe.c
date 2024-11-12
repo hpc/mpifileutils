dcd6e51336f48b6f8b5b8be5db58da00fbbf681b
Olaf Faaland <faaland1@llnl.gov>
2024-11-12 15:23:36 -0800

    Add (void) where we ignore mfu_flist_walk_param_paths() return value
    In places where we disregard the return value of mfu_flist_walk_param_paths(),
    add (void) as a visual indicator.
    
    In these cases we still need to analyze what we would do differently,
    if anything, with the knowledge that the walk failed.
    
    Signed-off-by: Olaf Faaland <faaland1@llnl.gov>

--- src/dstripe/dstripe.c
+++ src/dstripe/dstripe.c
@@ -553,7 +553,7 @@ int main(int argc, char* argv[])
     /* walk list of input paths and stat as we walk */
     mfu_flist flist = mfu_flist_new();
 
-    mfu_flist_walk_param_paths(numpaths, paths, walk_opts, flist, mfu_file);
+    (void) mfu_flist_walk_param_paths(numpaths, paths, walk_opts, flist, mfu_file);
 
     /* filter down our list to files which don't meet our striping requirements */
     mfu_flist filtered = filter_list(flist, stripes, stripe_size, min_size, &create_prog_count_total, &stripe_prog_bytes_total);
